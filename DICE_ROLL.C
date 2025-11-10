c2f1ad1c0fc5eb5344aa16628a25e6b818a8811b
Shivam-1501 <shivamxo9872@gmail.com>
2025-11-10 13:26:10 +0530

    Update DICE_ROLL.C

--- DICE_ROLL.C
+++ DICE_ROLL.C
@@ -1,26 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-int a,b,c=1;
-printf("	DICE ROLLING SIMULATOR		");
-while(c==1)
-{
-printf("1.Roll the dice\n");
-printf("2.Exit\n");
-printf("Enter your choice:");
-scanf("%d",&b);
-switch(b)
-{
-case 1:
-a=(rand()%6)+1;
-printf("%d\n",a);
-break;
-case 2:
-c=0;
-printf("Exiting the program....\n");
-break;
-}
-}
-return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int dice, rollAgain;
+
+    // Seed the random number generator with the current time
+    srand(time(0));
+
+    printf("🎲 Welcome to the Dice Rolling Simulator! 🎲\n");
+
+    do {
+        dice = (rand() % 6) + 1; // Generates a random number between 1 and 6
+        printf("\nYou rolled: %d\n", dice);
+
+        printf("\nRoll again? (1 = Yes, 0 = No): ");
+        scanf("%d", &rollAgain);
+
+    } while (rollAgain == 1);
+
+    printf("\nThanks for playing! Goodbye!\n");
+    return 0;
 }
